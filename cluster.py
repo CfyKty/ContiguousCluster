a26e43ef9aa7299eb128f6187c266a94b81f32f9
Marco Pretorius <mpre8@student.monash.edu>
2020-03-31 00:20:11 +1300

    Should be complete: -Added config functionality to cluster generation. -Improved cluster generation interface.
    TODO:
    -Add commandline arguments
    -Test

--- cluster.py
+++ cluster.py
@@ -5,16 +5,25 @@ try:
         config = json.load(f)
 
     cluster_size = int(config['cluster'])
+    sector_size = int(config['sector'])
+    FAT_start = int(config['fat_start'])
+
+    bytes_per_cluster = int(config['bytes_per_cluster'])
+    EOF = ("F"*bytes_per_cluster) #EOF marker.
 
 except FileNotFoundError:
     cluster_size = int(input("Enter cluster size: "))
+    EOF = "FFFFFFFF"
 
 start = int(input("Enter start: "), 16)
 size = int(input("Enter size: "), 16)
 
 cluster = ""
 clusters_to_write = math.ceil(size / cluster_size)
-print(clusters_to_write)
+print("\n Writing "+clusters_to_write+"clusters.")
+
+FAT_location = FAT_start * sector_size #fat 32 964 * 512
+address = (start * bytes_per_cluster) + FAT_location
 
 for outp in range(start + 1, start + clusters_to_write):
     ba = bytearray.fromhex("{0:08x}".format(outp)) #converts to hex byte array
@@ -22,4 +31,5 @@ for outp in range(start + 1, start + clusters_to_write):
     hexdigit = ''.join(format(x, '02x') for x in ba)  # constructs hex number from byte array
     cluster += hexdigit.upper()							#formats hex "Letters"
 
-print(cluster)
+print("\nPaste at address: 0x"+address+"\n")
+print(cluster + EOF)
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 import json
 
-start = int(input("Enter base10 custer area start: "))
-reserved = int(input("Enter reserved: "))
+fat_start = int(input("Enter FAT start: "))
+bytes_per_cluster = int(input("Enter bytes used per cluster (4 for FAT32): "))
+start = int(input("Enter base10 cluster area start: "))
+reserved = int(input("Enter reserved (2 for FAT32): "))
 cluster_size = int(input("Enter cluster size: "))
 sector_size = int(input("Enter sector size: "))
 
-config = {'reserved': reserved, 'start': start, 'cluster': cluster_size, 'sector': sector_size}
+config = {'fat_start':fat_start,'bytes_per_cluster':bytes_per_cluster,'reserved': reserved,'start': start, 'cluster': cluster_size, 'sector': sector_size}
 
 with open('config.json', 'w') as f:
     json.dump(config, f)
